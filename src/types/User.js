286e37428e62154decf05c7ec6c96f975580924e
Mark <ext_mchakravarthi@Falabella.cl>
2023-04-26 12:22:16 +0530

    added outdir

--- src/types/User.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-const typebox_1 = require("@sinclair/typebox");
-exports.User = typebox_1.Type.Object({
-    name: typebox_1.Type.String(),
-    mail: typebox_1.Type.Optional(typebox_1.Type.String({ format: "email" })),
-});
